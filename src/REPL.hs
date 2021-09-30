8a2bebc568b749211fa0ccbf19701d52faf4f71c
Jonas Carpay <jonascarpay@gmail.com>
2021-09-30 13:41:17 +0900

    Refactor REPL, print module on empty string

--- src/REPL.hs
+++ src/REPL.hs
@@ -1,4 +1,4 @@
-module REPL where
+module REPL (runRepl) where
 
 import AST
 import Codegen
@@ -7,6 +7,21 @@ import Data.ByteString.Char8 qualified as BS8
 import LLVM.Bindings (ContextRef, ModuleRef, moduleDump, valueDump)
 import Parser (parse)
 import System.Console.Haskeline
+
+handleInput :: ContextRef -> ModuleRef -> String -> IO ()
+handleInput ctx mdl = go
+  where
+    go "" = moduleDump mdl
+    go (':' : cmd) = putStrLn cmd
+    go str =
+      case parse (BS8.pack str) of
+        Left err -> putStrLn err
+        Right top -> do
+          case top of
+            TLDef def -> genFunction ctx mdl def >> moduleDump mdl
+            TLProto proto -> genProto ctx mdl proto >> moduleDump mdl
+            TLExpr expr -> withBuilder ctx $ \bld -> genExpr ctx mdl bld mempty expr >>= valueDump
+          putStrLn ""
 
 runRepl :: IO ()
 runRepl =
@@ -21,13 +36,4 @@ runRepl =
           minput <- getInputLine "Vandelay Industries> "
           case minput of
             Nothing -> outputStrLn "Goodbye"
-            Just input ->
-              case parse (BS8.pack input) of
-                Left str -> outputStrLn str >> go
-                Right top -> do
-                  liftIO $ case top of
-                    TLDef def -> genFunction ctx mdl def >> moduleDump mdl
-                    TLProto proto -> genProto ctx mdl proto >> moduleDump mdl
-                    TLExpr expr -> withBuilder ctx $ \bld -> genExpr ctx mdl bld mempty expr >>= valueDump
-                  outputStrLn ""
-                  go
+            Just input -> liftIO (handleInput ctx mdl input) >> go
